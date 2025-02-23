3bdecdd1d49c631facae7ca5c3c2b396e8f1511c
ahmed attafi <ahmed_attaffi@hotmail.fr>
2025-02-23 16:25:54 +0100

    final project

---
+++ GoMyCode/projet/res/script.js
@@ -0,0 +1,19 @@
+//alert('attention')
+
+//prompt('marhbe bik')
+//confirm('comment cv')
+//console.log('message danger')
+//console.log(typeof(true)) // imprime un booléen
+//console.log(typeof(9000)) // imprime un nombre
+//console.log(typeof("Übermensch" )) // imprime une chaîne de caractères
+//console.log(typeof(anUndefinedVar)) // imprime undefined
+//console.log(typeof(null)) // imprime l'objet
+var a ='my name is'
+var b= 24
+console.log (`${a} hamma my age is${b}`)
+function add(){
+
+}
+//var tab = [ 9 , 'hello' , true , 10]
+//console .log( tab[0])
+var
